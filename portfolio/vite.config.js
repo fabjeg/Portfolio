86072c9dd256f998437b2ae20b729c22f78d91c1
Jego <fabien.jego.p@gmail.com>
2024-12-01 12:05:46 +0100

    deploy

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/"
-})
+  base: "/Portfolio/", // Configure le chemin de base pour GitHub Pages
+  build: {
+    outDir: "dist", // Dossier de sortie
+    assetsDir: "assets", // Dossier des ressources (CSS, JS)
+  },
+});
