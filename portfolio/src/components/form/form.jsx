c545e5db5a0e0547e7671f0cbe24aee0e6985d08
Jego <fabien.jego.p@gmail.com>
2024-12-03 19:43:16 +0100

    implement emailjs

--- portfolio/src/components/form/form.jsx
+++ portfolio/src/components/form/form.jsx
@@ -1,24 +1,41 @@
 import { useForm } from "react-hook-form";
 import './style.css';
+import emailjs from 'emailjs-com';
+
 
 export function Form() {
+
   const {
     register,
-    handleSubmit,
+    // handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  // const onSubmit = (data) => console.log(data);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log("===>", e.target);
+    
+    emailjs.sendForm('service_zlwfo2c', 'template_jqfm4xp', e.target, '8Xw1Qhsh1n-FKPfu5')
+        .then((result) => {
+            console.log('Email sent:', result.text);
+        })
+        .catch((error) => {
+            console.error('Error sending email:', error);
+        });
+};
 
   return (
     <div className="form">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={sendEmail}>
         <div className="form-group">
           <div className="form-control">
             <input
               className={`input-form ${errors.Name ? "input-error" : ""}`}
               {...register("Name", { required: "Le nom est requis." })}
-              placeholder="Nom"
+              placeholder="nom"
+              name="to_name"
             />
             {errors.Name && <p className="error-message">{errors.Name.message}</p>}
           </div>
@@ -62,10 +79,11 @@ export function Form() {
             className={`textarea-form ${errors.Message ? "input-error" : ""}`}
             {...register("Message", { required: "Le message est requis." })}
             placeholder="Votre message"
+            name="message"
           />
           {errors.Message && <p className="error-message">{errors.Message.message}</p>}
         </div>
-        <input type="submit" value="Envoyer" className="submit-button" />
+        <button type="submit" value="Envoyer" className="submit-button"/>
       </form>
     </div>
   );
--- portfolio/src/components/header/header.jsx
+++ portfolio/src/components/header/header.jsx
@@ -1,28 +1,18 @@
 import "./style.css";
 import { useNavigate } from "react-router-dom";
-import { saveAs } from "file-saver";
 
 export function Header() {
   const navigate = useNavigate();
-
-  const handleDownload = () => {
-    const icon = document.querySelector(".fa-download");
-    if (icon) {
-      icon.classList.add("rotate");
-    }
-    saveAs("/CV/C_V_Fabien_JEGO.pdf", "C_V_Fabien_JEGO.pdf");
-    setTimeout(() => {
-      if (icon) icon.classList.remove("rotate");
-    }, 1500);
-  };
 
   return (
     <div>
       <div className="section-header">
         <div className="container-button">
           <div className="button-cv">
-            <div className="button" onClick={handleDownload}>
-              <i className="fa-solid fa-download" ></i>
+            <div className="button">
+              <a href="../../../../C_V_Fabien_JEGO.jpg" download="C_V_Fabien_JEGO.jpg">
+                <i className="fa-solid fa-download"></i>
+              </a>
             </div>
             <div className="cv">
               <p>Mon CV</p>
@@ -31,34 +21,17 @@ export function Header() {
           </div>
         </div>
         <div className="container-header-button">
-          <button
-            onClick={() => navigate("/")}
-            className="button-header  logo"
-          >
-            <i className="fa-solid fa-house"></i>
-            Home
-            
+          <button onClick={() => navigate("/")} className="button-header logo">
+            <i className="fa-solid fa-house"></i> Home
           </button>
-          <button
-            onClick={() => navigate("/about")}
-            className="button-header  logo"
-          >
-            <i className="fa-solid fa-user"></i>
-            À propos
+          <button onClick={() => navigate("/about")} className="button-header logo">
+            <i className="fa-solid fa-user"></i> À propos
           </button>
-          <button
-            onClick={() => navigate("/project")}
-            className="button-header  logo"
-          >
-            <i className="fa-solid fa-diagram-project"></i>
-            Projet
+          <button onClick={() => navigate("/project")} className="button-header logo">
+            <i className="fa-solid fa-diagram-project"></i> Projet
           </button>
-          <button
-            onClick={() => navigate("/contact")}
-            className="button-header logo"
-          >
-            <i className="fa-solid fa-address-card"></i>
-            Contact
+          <button onClick={() => navigate("/contact")} className="button-header logo">
+            <i className="fa-solid fa-address-card"></i> Contact
           </button>
         </div>
       </div>
