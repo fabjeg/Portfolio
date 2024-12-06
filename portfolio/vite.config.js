20151cddf34567dc257d9c7b18b35cdadb78249f
Jego <fabien.jego.p@gmail.com>
2024-12-06 12:07:55 +0100

    deploy

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/",
+  base: "/Portfolio/", // Configure le chemin de base pour GitHub Pages ou Vercel
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js', 'mjs'],
+    },
+  },
 });
