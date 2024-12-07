ef75baf9136bc1228a67c5749ba2b8ae035b82a2
Jego <fabien.jego.p@gmail.com>
2024-12-07 12:16:35 +0100

    refactoring form

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -12,18 +12,18 @@ export function Form() {
     handleSubmit,
     formState: { errors, isValid },
     reset,
-  } = useForm({ mode: "onTouched" }); // Activation de la validation dès que le champ est touché
+  } = useForm({ mode: "onTouched" });
+  
   const [message, setMessage] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const sendEmail = (data) => {
-    console.log("Data submitted:", data); // Debug pour vérifier les valeurs
     emailjs
       .send(
         "service_zlwfo2c",
         "template_jqfm4xp",
         {
-          to_name: data.to_name,
+          from_name: data.from_name,
           from_email: data.from_email,
           entreprise: data.entreprise,
           sujet: data.sujet,
@@ -32,13 +32,13 @@ export function Form() {
         "8Xw1Qhsh1n-FKPfu5"
       )
       .then((result) => {
-        console.log("Email sent:", result.text);
+        console.log("Email envoyé avec succès :", result);
         setMessage("Votre message a bien été envoyé !");
         setIsModalOpen(true);
-        reset(); // Réinitialise les champs après l'envoi
+        reset();
       })
       .catch((error) => {
-        console.error("Error sending email:", error);
+        console.error("Erreur lors de l'envoi :", error);
         setMessage("Une erreur est survenue lors de l'envoi de votre message.");
         setIsModalOpen(true);
       });
@@ -51,37 +51,28 @@ export function Form() {
   return (
     <div className={`form ${theme}`}>
       <form onSubmit={handleSubmit(sendEmail)}>
-        {/* Message global d'erreur */}
-        {Object.keys(errors).length > 0 && (
-          <p className="global-error">
-            Veuillez corriger les erreurs ci-dessous avant de soumettre le
-            formulaire.
-          </p>
+        {isModalOpen && (
+          <p className="global-error">Veuillez remplir correctement tous les champs requis.</p>
         )}
 
         <div className="form-group">
           <div className="form-control">
-            <label htmlFor="to_name">Nom</label>
+            <label htmlFor="from_name">Nom</label>
             <input
-              id="to_name"
-              className={`input-form ${
-                errors.to_name ? "input-error" : ""
-              } ${theme}`}
-              {...register("to_name", { required: "Le nom est requis." })}
+              id="from_name"
+              className={`input-form ${errors.from_name ? "input-error" : ""}`}
+              {...register("from_name", { required: "Le nom est requis." })}
               placeholder="Entrez votre nom"
-              name="from_name"
             />
-            {errors.to_name && (
-              <p className="error-message">{errors.to_name.message}</p>
+            {errors.from_name && (
+              <p className="error-message">{errors.from_name.message}</p>
             )}
           </div>
           <div className="form-control">
             <label htmlFor="from_email">Email</label>
             <input
               id="from_email"
-              className={`input-form ${
-                errors.from_email ? "input-error" : ""
-              } ${theme}`}
+              className={`input-form ${errors.from_email ? "input-error" : ""}`}
               {...register("from_email", {
                 required: "L'email est requis.",
                 pattern: {
@@ -102,12 +93,8 @@ export function Form() {
             <label htmlFor="entreprise">Entreprise</label>
             <input
               id="entreprise"
-              className={`input-form ${
-                errors.entreprise ? "input-error" : ""
-              } ${theme}`}
-              {...register("entreprise", {
-                required: "Le nom de l'entreprise est requis.",
-              })}
+              className={`input-form ${errors.entreprise ? "input-error" : ""}`}
+              {...register("entreprise", { required: "Le nom de l'entreprise est requis." })}
               placeholder="Entrez le nom de l'entreprise"
             />
             {errors.entreprise && (
@@ -118,9 +105,7 @@ export function Form() {
             <label htmlFor="sujet">Sujet</label>
             <input
               id="sujet"
-              className={`input-form ${
-                errors.sujet ? "input-error" : ""
-              } ${theme}`}
+              className={`input-form ${errors.sujet ? "input-error" : ""}`}
               {...register("sujet", { required: "Le sujet est requis." })}
               placeholder="Entrez le sujet"
             />
@@ -134,9 +119,7 @@ export function Form() {
           <label htmlFor="message">Message</label>
           <textarea
             id="message"
-            className={`textarea-form ${
-              errors.message ? "input-error" : ""
-            } ${theme}`}
+            className={`textarea-form ${errors.message ? "input-error" : ""}`}
             {...register("message", { required: "Le message est requis." })}
             placeholder="Entrez votre message"
           />
@@ -148,11 +131,12 @@ export function Form() {
         <button
           type="submit"
           className="submit-button"
-          disabled={!isValid} // Désactivation du bouton si le formulaire n'est pas valide
+          disabled={!isValid} 
         >
           Envoyer
         </button>
       </form>
+
       {isModalOpen && (
         <ConfirmationModal message={message} onClose={closeModal} />
       )}
